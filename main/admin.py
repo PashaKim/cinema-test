70c9893af8f3eb00c4ece9785caaa7c8fbdd31ff
pavlo <pavlo.chubarov.ua@gmail.com>
2020-03-23 14:14:15 +0200

    add templates

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Menu, MenuElement
 
-# Register your models here.
+
+class MenuElementInLines(admin.TabularInline):
+    model = MenuElement
+    extra = 2
+    readonly_fields = ('created', 'updated')
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name','elements', 'created', 'updated')
+    readonly_fields = ('created', 'updated')
+    inlines = [MenuElementInLines]
+
+
+@admin.register(MenuElement)
+class MenuElement(admin.ModelAdmin):
+    list_display = ('menu', 'name', 'url', 'created', 'updated')
+    list_filter = ('menu',)
+    readonly_fields = ('created', 'updated')
+
--- main/models.py
+++ main/models.py
@@ -3,12 +3,23 @@ from django.db import models
 
 class Menu(models.Model):
     name = models.CharField(max_length=64)
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
-class Element(models.Model):
+    @property
+    def elements(self):
+        return list(self.menuelement_set.values_list('name', 'url'))
+
+
+class MenuElement(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     url = models.CharField(max_length=128, default='/')
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
